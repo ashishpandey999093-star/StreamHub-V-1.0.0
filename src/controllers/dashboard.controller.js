60f19036538cfc76f01b4ef1a0ef9a1289f4168a
ashish_0002 <ashishpandey999093@gmail.com>
2026-08-07 22:05:24 +0530

    fix: correct import casing for asyncHandler in dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -4,7 +4,7 @@ import { Subscription } from "../models/subscription.model.js"
 import { Like } from "../models/like.model.js"
 import ApiError from "../utils/ApiError.js"
 import ApiResponse from "../utils/ApiResponse.js"
-import asyncHandler from "../utils/asyncHandler.js"
+import asyncHandler from "../utils/asynchandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     const userId = req.user._id;
