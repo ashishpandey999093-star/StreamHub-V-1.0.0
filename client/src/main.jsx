8af39a908211f9d7f9dcdbc85abdb09601796db1
ashish_0002 <ashishpandey999093@gmail.com>
2026-08-09 09:26:18 +0530

    feat: add @vercel/analytics dependency and integrate analytics component in main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
@@ -13,5 +14,6 @@ createRoot(document.getElementById("root")).render(
         <RouterProvider router={router} />
       </AuthProvider>
     </ThemeProvider>
+     <Analytics />
   </StrictMode>
 );
